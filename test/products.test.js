376af2edec1b8b9e99da2cddfc6b216a1b598d6e
mjabur <a95576@aup.edu>
2019-12-14 22:34:48 +0000

    updata

--- test/products.test.js
+++ test/products.test.js
@@ -1,59 +1,82 @@
-let request = require('supertest')
-let app = require('../app')
-let expect = require('../models/product').expect
-
+const request = require('supertest')
+const expect = require('chai').expect
+const app = require('../app')
+const Product = require('../models/product')
+const User = require('../models/user')
 
 let user = {
-_id: '3ddcfa78e38c7107015f35b6',
-name: 'User',
-password: 'pass',
-email: 'a@b.c'
+  _id: '3ddcfa78e38c7107015f35b6',
+  name: 'MO',
+  password: 'jabur',
+  email: 'mo@jabur.com'
 }
 let product1 = {
-_id: '5ddcfa78e38c7107015f35b6',
-name: "Printer",
-user: user
+  _id: '7ddcfa77e66c7107015f35b6',
+  name: "Football",
+  user: user
 }
 let product2 = {
-_id: '4ddcfa78e38c7107015f35b6',
-name: "Screen",
-user: user
+  _id: '5ddcfa734e54c7107015f35b6',
+  name: "Tinnes",
+  user: user
 }
 
-describe('Getting products', () => {
-    it('should return the name and ids of all products', async () => {
-        const res = await request(app)
-            .get('/api/products')
-              expect(res.statusCode).equals(200)
-        expect(res.body).to.have.nested.property('data[0].name','Printer')
-        expect(res.body).to.have.nested.property('data[1].name','Laptop')
-    })
-    it('should return the name and id of a certain product', async () => {
-        const res = await request(app)
-            .get('/api/products/1')
-        expect(res.statusCode).equals(200)
-        expect(res.body).to.have.nested.property('data[0].name','Printer') 
-    })
-})
+describe('Products', () => {
+  beforeEach(async function() {
+    let u = new User(user)
+    await u.save()
+  });
+  afterEach(async function() {
+    await User.deleteOne({_id: user._id})
+	});
 
-describe('Creating products', () => {
-    it('should create correctly', async () => {
-        let newProduct = {
-            name: "Eraser"
-    }
-    var res = await request(app)
-    .post('/api/products')
-    .send(newProduct)
-    expect(res.statusCode).equals(201)
-    expect(res.body).to.have.property('data').
-    to.have.property('message','Created ok')
-    expect(res.body).to.have.property('data')
-    .to.have.property('id')
-const id = res.body.data.id
-res = await request(app)
-.get(`/api/products/${id}`)
-expect(res.statusCode).equals(200)
-expect(res.body).
-to.have.nested.property('data[2].name','Eraser')
-        })
+  describe('Getting products', () => {
+    beforeEach(async function() {
+      let p1 = new Product(product1)
+      let p2 = new Product(product2)
+
+      await p1.save()
+      await p2.save()
+    });
+    afterEach(async function() {
+      await Product.deleteOne({_id: product1._id})
+      await Product.deleteOne({_id: product2._id})
+    });
+
+    it('should return all products', async () => {
+        const res = await request(app)
+            .get(`/api/${user._id}/products`)
+        expect(res.statusCode).equals(200)
+        expect(res.body).to.have.nested.property('data[0].name', 'Printer')
+        expect(res.body).to.have.nested.property('data[1].name', 'Screen')
+    })
+  })
+
+  describe('Creating products', () => {
+    afterEach(async function() {
+      await Product.deleteOne({_id: product1._id})
+    });
+
+      it('should create correctly and return id and message', async () => {
+          var res = await request(app)
+            .post(`/api/${user._id}/products`)
+            .send(product1)
+          expect(res.statusCode).equals(201)
+          expect(res.body).to.have.property('data').to.have.property('message','Created ok')
+          expect(res.body).to.have.property('data').to.have.property('id')
+          const id = res.body.data.id
+          res = await request(app)
+            .get(`/api/${user._id}/products/${id}`)
+          console.log(JSON.stringify(res.body))
+          expect(res.statusCode).equals(200)
+          expect(res.body).to.have.nested.property('data.name', 'Printer')
+      })
+  })
 })
+exports.init = async function() {
+    try {
+        await mongoose.connect(env.db, {useNewUrlParser: true, useUnifiedTopology: true});
+    } catch (error) {
+        console.log(error)
+    }
+}
--- test/users.test.js
+++ test/users.test.js
@@ -1,16 +1,16 @@
-let request = require('supertest')
-let app = require('../app')
-let expect = require('chai').expect
+const request = require('supertest')
+const expect = require('chai').expect
+const app = require('../app')
+const User = require('../models/user')
 
 describe('Getting users', () => {
     it('should return the name and ids of all users', async () => {
         const res = await request(app)
             .get('/api/users')
         expect(res.statusCode).equals(200)
-        expect(res.body).to.have.property('data')
-            .to.deep.include({id: 1, name: "Jack"}) 
-        expect(res.body).to.have.property('data')
-            .to.deep.include({id: 2, name: "Sam"}) 
+        expect(res.body).to.have.property('data').to.deep.include({id: 1, name: "Mo"}) 
+         expect(res.body).to.have.property('data').to.deep.include({id: 1, name: "Jack"}) 
+        expect(res.body).to.have.property('data').to.deep.include({id: 2, name: "Sam"}) 
     })
     it('should return the name and id of a certain user', async () => {
         const res = await request(app)
@@ -19,3 +19,10 @@ describe('Getting users', () => {
         expect(res.body).to.have.property('data').to.deep.equal({id: 1, name: "Sarah"}) 
     })
 })
+exports.init = async function() {
+    try {
+        await mongoose.connect(env.db, {useNewUrlParser: true, useUnifiedTopology: true});
+    } catch (error) {
+        console.log(error)
+    }
+}
